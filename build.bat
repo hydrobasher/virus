3860f03573ad3b37cc70ae9e2846b1cc3f3afeff
Hydrobasher <luanreyneke@gmail.com>
2026-02-07 21:06:35 +0200

    fixed build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,3 @@
 @echo off
-nasm -f win64 helper.asm -o helper.obj
-gcc -c main.c -m64 -o main.obj
-gcc main.obj helper.obj -o out.exe -mwindows -luser32 -lgdi32
-.\out
-del *.obj
-del out.exe
+g++ virus.cpp -o virus.exe -luser32 -lwinmm -lole32 -lxaudio2_8 -mwindows
+start "" .\virus.exe
